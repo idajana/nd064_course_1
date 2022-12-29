c6a595c707a25b7da0ce4def25f833fb6d4ee01b
idajana <ivkic.dajana@gmail.com>
2022-12-29 18:37:24 +0100

    fix: improve app logging

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,5 +1,6 @@
 import sqlite3
-
+import sys
+import logging
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
@@ -110,4 +111,12 @@ def metrics():
 
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    # set logger to handle STDOUT and STDERR
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stderr_handler = logging.StreamHandler(sys.stderr)
+    handlers = [stderr_handler, stdout_handler]
+    # format output
+    format_output = '%(asctime)s - %(levelname)s - %(message)s'
+
+    logging.basicConfig(format=format_output, level=logging.DEBUG, handlers=handlers)
+    app.run(host='0.0.0.0', port='3111')
